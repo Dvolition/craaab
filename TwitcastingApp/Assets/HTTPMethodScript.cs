14d268a91872b10f984282038870d2d385039b3f
坂本　大介 <sailingoflife@gmail.com>
2017-01-28 01:04:33 +0900

    init commit

---
+++ TwitcastingApp/Assets/HTTPMethodScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HTTPMethodScript : MonoBehaviour {
+
+    public string msg;
+    public GameObject LoadingPanel;
+
+
+    public void SendMessageMethod(string name)
+    {
+        msg = name;
+        StartCoroutine(SendMessage());
+        LoadingPanel.SetActive(true);
+    }
+
+    public IEnumerator SendMessage()
+    {
+
+        string url = "https://script.google.com/macros/s/AKfycbzRiGiNiUJRjx1Gmi5cxzwYZpXfBwH7KZvhLz6JuZ3jHy2ha5E/exec";
+
+        WWWForm form = new WWWForm();
+        form.AddField("user_name",msg);
+
+
+        WWW www = new WWW(url, form);
+        yield return www;
+
+        yield return new WaitForSeconds(2f);
+        LoadingPanel.SetActive(false);
+    }
+}
---
+++ TwitcastingApp/Assets/LoadingPanelScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadingPanelScript : MonoBehaviour {
+
+    [SerializeField]
+    private Text loadText;
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AnimationStep()
+    {
+        loadText.text += ".";
+        if(loadText.text == "Now Loading ....")
+        {
+            loadText.text = "Now Loading ";
+        }
+    }
+}

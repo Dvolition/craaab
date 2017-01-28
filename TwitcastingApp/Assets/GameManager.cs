28ea6438971a62f0a29184b04e842be9c01681be
坂本　大介 <sailingoflife@gmail.com>
2017-01-28 12:10:00 +0900

    no message

---
+++ TwitcastingApp/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    public Player myPlayer;
+
+    public List<Player> players;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+}
+
+[System.Serializable]
+public class Player{
+    public string name;
+    public Sprite iconImage; 
+    public string job; //役職
+    public string log; //最後の行動履歴
+    public bool isLive; //今生きているか
+}
---
+++ TwitcastingApp/Assets/Managers.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Managers : MonoBehaviour {
+
+    public static Managers instance;
+
+    public HTTPMethodScript httpMethodScript;
+
+
+
+	// Use this for initialization
+	void Start () {
+        if( instance == null ){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            GameObject.Destroy(gameObject);
+        }
+
+	}
+
+}
